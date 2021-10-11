dee2b6df6c5d3656558f17b98103d97edd17a8a4
mucahitsndc <mucahit.sndc@gmail.com>
2021-10-11 15:39:11 +0300

    password reset host test

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -48,7 +48,7 @@ class ApiController extends Controller
                 'access'=>md5((Setting::where('setting','access')->first(['option']))->option),
                 'type'=>'resetaccess'
             ]);
-            $token=$crypt->crypt_router($token,true,'encode',2180);
+            $token=$crypt->crypt_router($token,true,'encode',180);
             $data = [
                 'link'=>env('APP_FRONT_URL').'/yonetici/parola-sifirla/'.$token,
                 'email'=>(MainInfo::where('title','email')->where('status',0)->first(['info']))->info
